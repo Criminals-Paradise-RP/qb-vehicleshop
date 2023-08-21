f874ad7d2d817de7ceb335829c47cb3eb51c279a
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-20 17:45:45 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -118,20 +118,20 @@ RegisterNetEvent('qb-vehicleshop:server:swapVehicle', function(data)
     TriggerClientEvent('qb-vehicleshop:client:homeMenu', src)-- reopen main menu
 end)
 
--- Send customer for test drive
-RegisterNetEvent('qb-vehicleshop:server:customTestDrive', function(vehicle, playerid)
-    local src = source
-    local target = tonumber(playerid)
-    if not QBCore.Functions.GetPlayer(target) then
-        TriggerClientEvent('QBCore:Notify', src, Lang:t('error.Invalid_ID'), 'error')
-        return
-    end
-    if #(GetEntityCoords(GetPlayerPed(src)) - GetEntityCoords(GetPlayerPed(target))) < 3 then
-        TriggerClientEvent('qb-vehicleshop:client:customTestDrive', target, vehicle)
-    else
-        TriggerClientEvent('QBCore:Notify', src, Lang:t('error.playertoofar'), 'error')
-    end
-end)
+-- -- Send customer for test drive
+-- RegisterNetEvent('qb-vehicleshop:server:customTestDrive', function(vehicle, playerid)
+--     local src = source
+--     local target = tonumber(playerid)
+--     if not QBCore.Functions.GetPlayer(target) then
+--         TriggerClientEvent('QBCore:Notify', src, Lang:t('error.Invalid_ID'), 'error')
+--         return
+--     end
+--     if #(GetEntityCoords(GetPlayerPed(src)) - GetEntityCoords(GetPlayerPed(target))) < 3 then
+--         TriggerClientEvent('qb-vehicleshop:client:customTestDrive', target, vehicle)
+--     else
+--         TriggerClientEvent('QBCore:Notify', src, Lang:t('error.playertoofar'), 'error')
+--     end
+-- end)
 
 -- Make a finance payment
 RegisterNetEvent('qb-vehicleshop:server:financePayment', function(paymentAmount, vehData)
